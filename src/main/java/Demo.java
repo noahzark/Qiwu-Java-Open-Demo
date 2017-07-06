46da7ef97fa94578b5c8ed406da1be22244be436
Feliciano Long <noahzark@vip.qq.com>
2017-07-05 20:59:55 -0500

    initial import

---
+++ src/main/java/Demo.java
@@ -0,0 +1,60 @@
+import okhttp3.*;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.util.Date;
+import java.util.Scanner;
+
+public class Demo {
+
+    OkHttpClient client = new OkHttpClient();
+    MediaType mediaType = MediaType.parse("application/x-www-form-urlencoded");
+
+    // App information
+    String server = "https://robot-service.centaurstech.com/api/chat";
+    String appkey = "open-demo";
+    String appsecret = "123456789";
+    String nickname = "张三李四王老五";
+
+    // Get hardware fingerprint
+    String uid = GetNetworkAddress.GetAddress("mac");
+
+    public String getResponse(String ask) throws Exception {
+        // Prepare verify string
+        String now = (new Date()).getTime() + "";
+        String verify = Md5.digest(appsecret + uid + now);
+
+        // Generate request body
+        String bodyStr = String.format(
+                "appkey=%s&timestamp=%s&uid=%s&verify=%s&msg=%s&nickname=%s",
+                appkey, now, uid, verify, ask, nickname);
+
+        RequestBody body = RequestBody.create(mediaType,
+                bodyStr);
+        Request request = new Request.Builder()
+                .url(server)
+                .post(body)
+                .addHeader("content-type", "application/x-www-form-urlencoded")
+                .addHeader("cache-control", "no-cache")
+                .build();
+
+        Response response = client.newCall(request).execute();
+        JSONObject json = new JSONObject(response.body().string());
+        return json.getString("msg");
+    }
+
+    public static void main(String[] args) throws Exception {
+        Demo demo = new Demo();
+        String message = demo.getResponse("HELLO");
+        System.out.println(message);
+
+        Scanner sc = new Scanner(System.in);
+        while (true) {
+            message = sc.nextLine();
+            message = demo.getResponse(message);
+            System.out.println(message);
+        }
+
+    }
+
+}
